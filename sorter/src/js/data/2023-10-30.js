d3006f4b0fd05b3a05422a4974b1f0bfd28eeed0
ameliaRTA <19891857+ameliaRTA@users.noreply.github.com>
2023-10-30 17:04:58 +0100

    Add files via upload

---
+++ sorter/src/js/data/2023-10-30.js
@@ -0,0 +1,298 @@
+dataSetVersion = "2023-10-30"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Series Entry",
+    key: "series",
+    tooltip: "Check this to restrict to certain series.",
+    checked: true,
+    sub: [
+      { name: "Birthday", key: "bday" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Hina (Blue Archive)",
+    img: "3JOTTZb.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Shiroko Terror (Blue Archive)",
+    img: "ktBa8rd.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Surtr (Arknights)",
+    img: "EcSfZ06.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Nagatoro",
+    img: "7y7U79O.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Mahiro (Onimai)",
+    img: "SHTUwA1.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Mafuyo (Bokuben)",
+    img: "DmV9q2l.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Watamelon",
+    img: "RrsChb4.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Mio (Xenoblade)",
+    img: "DwLgYE6.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Mecha-Fiora (Xenoblade)",
+    img: "dtsBMaj.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Blackbeard (FGO)",
+    img: "JHtmkUq.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Mococo (Hololive)",
+    img: "G1bJtr7.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Mutsuki (Blue Archive)",
+    img: "EF3RNZ7.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Chicken (100% Orange Juice)",
+    img: "tpAvz3G.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Yui (Angel Beats)",
+    img: "51G81pC.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Turtle (idk man)",
+    img: "6mFPm0C.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "John Cena (Bing Chilling)",
+    img: "D7yP82j.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Koyuki Spehere (Blue Archive)",
+    img: "oM9juxj.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Topaz (Honkai Star Rail)",
+    img: "V96IZRJ.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Furina (Genshin Impact)",
+    img: "Op8jNMZ.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "March 7th (Honkai Star Rail)",
+    img: "axVwIlg.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Shiroko (Blue Archive) iwak",
+    img: "MwKyx8H.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Hoshino (Blue Archive) pouch",
+    img: "uE97cIv.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Loona Griffin",
+    img: "6x66PQW.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Max Fosh (Youtuber)",
+    img: "TU2ZVOM.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Gutsman.EXE (Mega Man)",
+    img: "GfJrCDz.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Pip Bernadotte (Hellsing)",
+    img: "KyzRayh.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Lafiel (Seikai no Series)",
+    img: "Ep8PcNT.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Angry Faic (Newgrounds)",
+    img: "lXCauuF.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Liz (FGO)",
+    img: "5uOPPTX.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Osakabehime (FGO)",
+    img: "Xpt0inp.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Saber (Fate)",
+    img: "kLRQIkj.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Shit I'm Gay (Guzma, Pokemon)",
+    img: "C21khP1.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Toki (Blue Archive) miaw",
+    img: "j4ZnM2o.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Hoshino (Blue Archive) miaw",
+    img: "bB5WCi5.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "hosh (Hoshino, Blue Archive)",
+    img: "9g4PBEM.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Kotori (Blue Archive)",
+    img: "wrBQ3jP.jpeg",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Toki (Blue Archive) dancing",
+    img: "RFlwDlP.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Aris (Blue Archive) dancing",
+    img: "yY0Dz4E.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Allmind (Armored Core)",
+    img: "DNNiieA.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+  {
+    name: "Faust (Limbus Company)",
+    img: "PYQHNt4.png",
+    opts: {
+      series: ["bday"]
+    }
+  },
+
+];
